405d9d5f3248fa68998c07065233c421d2f489b1
trhallam <trhallam@gmail.com>
2021-12-29 15:40:34 +0000

    remove refs to zip/tar for now

--- dataicer/__init__.py
+++ dataicer/__init__.py
@@ -4,8 +4,9 @@ from dataicer.plugins.file import BaseFileHandler
 from ._core import ice, deice, list_handlers
 from ._base_archive import BaseArchiveHandler
 from ._dir_archive import DirectoryHandler
-from ._tar_archive import TarHandler
-from ._zip_archive import ZipHandler
+
+# from ._tar_archive import TarHandler
+# from ._zip_archive import ZipHandler
 
 
 def register_handlers(
